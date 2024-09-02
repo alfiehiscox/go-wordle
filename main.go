a41ea0404a0b55a4632b576c7b40e3c42df4635e
Alfie Hiscox <alfie.hiscox@gmail.com>
2024-09-02 13:11:54 +0100

    simple hangman

--- main.go
+++ main.go
@@ -1,7 +1,69 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"unicode"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type Model struct {
+	target   []rune
+	progress []rune
+}
+
+func NewModel(target string) Model {
+
+	progress := make([]rune, len(target))
+	for i := range progress {
+		progress[i] = '_'
+	}
+
+	return Model{
+		target:   []rune(target),
+		progress: progress,
+	}
+}
+
+func (m Model) Init() tea.Cmd {
+	return nil
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		default:
+			s := msg.String()
+			if len(s) == 1 {
+				r := rune(s[0])
+				if unicode.IsLetter(r) {
+					for i, letter := range m.target {
+						if letter == r {
+							m.progress[i] = letter
+						}
+					}
+				}
+			}
+		}
+	}
+	return m, nil
+}
+
+func (m Model) View() string {
+	if string(m.progress) == string(m.target) {
+		return "Congrats you won"
+	}
+	return string(m.progress)
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	p := tea.NewProgram(NewModel("alfie"))
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Could not run program: %v\n", err)
+		os.Exit(1)
+	}
 }
