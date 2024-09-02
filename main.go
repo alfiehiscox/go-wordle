11f87a193c692940d59f084b6e8e5b05e90ec2fe
Alfie Hiscox <alfie.hiscox@gmail.com>
2024-09-02 13:41:40 +0100

    basic guessing game

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 	"unicode"
 
 	tea "github.com/charmbracelet/bubbletea"
@@ -10,19 +11,36 @@ import (
 
 type Model struct {
 	target   []rune
-	progress []rune
+	progress [][]rune
+
+	index   int
+	guess   int
+	guesses [][]rune
 }
 
 func NewModel(target string) Model {
 
-	progress := make([]rune, len(target))
+	progress := make([][]rune, len(target))
 	for i := range progress {
-		progress[i] = '_'
+		p := make([]rune, len(target))
+		for j := range p {
+			p[j] = '_'
+		}
+		progress[i] = p
+	}
+
+	guesses := make([][]rune, 5)
+	for i := range guesses {
+		guesses[i] = make([]rune, len(target))
 	}
 
 	return Model{
 		target:   []rune(target),
 		progress: progress,
+
+		index:   0,
+		guess:   0,
+		guesses: guesses,
 	}
 }
 
@@ -34,18 +52,32 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "ctrl+c", "q":
+		case "ctrl+c":
 			return m, tea.Quit
+		case "enter":
+			if m.guess > 0 {
+				copy(m.progress[m.guess], m.progress[m.guess-1])
+			}
+
+			for i, letter := range m.guesses[m.guess] {
+				if letter == m.target[i] {
+					m.progress[m.guess][i] = letter
+				}
+			}
+			m.index = 0
+			m.guess++
+		case "back":
+			if m.index > 0 {
+				m.guesses[m.guess][m.index] = '_'
+				m.index -= 1
+			}
 		default:
 			s := msg.String()
 			if len(s) == 1 {
 				r := rune(s[0])
 				if unicode.IsLetter(r) {
-					for i, letter := range m.target {
-						if letter == r {
-							m.progress[i] = letter
-						}
-					}
+					m.guesses[m.guess][m.index] = r
+					m.index += 1
 				}
 			}
 		}
@@ -54,10 +86,16 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m Model) View() string {
-	if string(m.progress) == string(m.target) {
-		return "Congrats you won"
+
+	b := strings.Builder{}
+	for i, guess := range m.guesses {
+		progress := m.progress[i]
+		if string(progress) == string(m.target) {
+			return "Congrats you won"
+		}
+		b.WriteString(string(guess) + "->" + string(progress) + "\n")
 	}
-	return string(m.progress)
+	return b.String()
 }
 
 func main() {
